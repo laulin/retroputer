3c7bb60f76ca414c8ba5aa80a1e14c407b8c0ed1
laulin <laulin@nomail.com>
2019-09-15 15:32:08 +0200

    add error definition file

---
+++ sources/error.h
@@ -0,0 +1,10 @@
+#ifndef __ERROR_HEADER__
+#define __ERROR_HEADER__
+
+// generic
+#define OK                  0
+#define KO                  1
+// SRAM error
+#define SRAM_OUT_OF_RANGE   2 
+
+#endif // __ERROR_HEADER__
