179b3ed7176abd124ebb9d2f93a63aa04e59d1b6
laulin <laulin@nomail.com>
2019-09-20 20:14:06 +0200

    add mock for spi

---
+++ sources/mock_spi.c
@@ -0,0 +1,77 @@
+#include "spi.h"
+
+// mock functions for spi
+
+void init_spi(void)
+{
+}
+
+uint8_t read_write_spi (uint8_t data)
+{
+    return 0;
+}
+
+void write_bytes_spi (const uint8_t* data, uint8_t size)
+{
+    
+}
+
+void read_bytes_spi (uint8_t* data, uint8_t size)
+{
+    
+}
+
+void clear_ram0_select(void)
+{
+    
+}
+
+void set_ram0_select(void)
+{
+    
+}
+
+void clear_ram1_select(void)
+{
+    
+}
+
+void set_ram1_select(void)
+{
+    
+}
+
+void clear_rom0_select(void)
+{
+    
+}
+
+void set_rom0_select(void)
+{
+    
+}
+
+void clear_rom1_select(void)
+{
+    
+}
+
+void set_rom1_select(void)
+{
+    
+}
+
+void clear_io_select(void)
+{
+    
+}
+
+void set_io_select(void)
+{
+    
+}
+
+void set_all_select(void)
+{
+
+}
